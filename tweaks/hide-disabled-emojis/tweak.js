17ad9bf49d12feafd9c6c6c8a7473b5a696e1e48
Juby210 <31005896+Juby210@users.noreply.github.com>
2021-03-25 10:18:38 +0100

    fix HideDisabledEmojis tweak

--- tweaks/hide-disabled-emojis/tweak.js
+++ tweaks/hide-disabled-emojis/tweak.js
@@ -10,9 +10,14 @@ module.exports = {
     enable: async () => {
         const mod = await getModule(['isEmojiFiltered']);
 
-        inject('discord-tweaks-hde', mod, 'isEmojiFiltered', args => mod.isEmojiDisabled(...args));
+        inject('discord-tweaks-hde', mod, 'isEmojiFiltered', (args, res) => res || mod.isEmojiDisabled(...args));
+        inject('discord-tweaks-hde2', mod, 'getEmojiUnavailableReason', (args, res) => {
+            if (args[0] && mod.isEmojiDisabled(args[0].emoji, args[0].channel, args[0].intention)) return 0;
+            return res;
+        });
     },
     disable: () => {
         uninject('discord-tweaks-hde');
+        uninject('discord-tweaks-hde2');
     }
 };
